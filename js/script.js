5f20a0f19c6688ca890d559767219870964f5ca1
Mwansa Chilekwa <mwansachille@gmail.com>
2022-07-31 03:04:32 +0200

    Create getComputerChoice() function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+function getComputerChoice(){
+    let options=['rock', 'paper', 'scissors'];
+    let choice=options[Math.floor(Math.random()*options.length)];
+    return choice;
+}
+console.log(getComputerChoice());
